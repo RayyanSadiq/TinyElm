8cdfcdb3c4794f5955e520d47dd8df665b19f464
DragonXdroid <raysadik5@gmail.com>
2024-02-07 00:58:47 -0600

    commit #3

--- main.lua
+++ main.lua
@@ -1,52 +1,130 @@
 function love.load()
 
-    love.window.setMode(1024,768)
-    love.graphics.setDefaultFilter( "nearest" )
+    anim8 = require 'libraries/anim8/anim8'
+    windField = require 'libraries/windfield/windfield'
 
-    sprites = {}
-    sprites.player = love.graphics.newImage('tiny-RPG-forest-files/PNG/sprites/hero/idle/hero-idle-front/hero-idle-front.png')
+    love.window.setMode(1024,768)
+    love.graphics.setDefaultFilter( "nearest", "nearest" )
+     
+
+    sti = require 'libraries/Simple-Tiled-Implementation/sti'
+    cameraFile = require 'libraries/hump/camera'
+
+    camera = cameraFile(nil,nil)
+
+    world = windField.newWorld()
+   
 
     
     player = {}
-    player.x = 100
-    player.y = 100
-    player.speed = 180
+    player.collider = world:newCircleCollider(0,0,40)
+    player.collider:setX(2700)
+    player.collider:setY(2000)
+    player.speed = 300
+    player.spriteSheet = love.graphics.newImage('tiny-RPG-forest-files/PNG/spritesheets/hero/walk/hero-walk-sheet.png')
+    player.grid = anim8.newGrid(32,32, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
+    player.direction = 'down'
 
+    player.animations = {}
+
+    -- idle animations
+    player.animations.idle = {}
+    player.animations.idle.up = anim8.newAnimation(player.grid('1-1', 1), 100)
+    player.animations.idle.down = anim8.newAnimation(player.grid('1-1', 2), 100)
+    player.animations.idle.right = anim8.newAnimation(player.grid('1-1', 3), 100)
+    player.animations.idle.left = anim8.newAnimation(player.grid('1-1',4),100)
+
+    -- walk animations
+    player.animations.walk = {}
+    player.animations.walk.up = anim8.newAnimation(player.grid('1-6', 5), 0.1)
+    player.animations.walk.down = anim8.newAnimation(player.grid('1-6', 6), 0.1)
+    player.animations.walk.right = anim8.newAnimation(player.grid('1-6', 7), 0.1)
+    player.animations.walk.left = anim8.newAnimation(player.grid('1-6', 8), 0.1)
+
+    player.currentAnimation = player.animations.idle.down
+
+    loadMap("TutorialMap")
 end
 
 function love.update(dt)
 
-    local moveX, moveY = 0, 0
+    local vectorX, vectorY = 0, 0
+    local isMoving = false
 
     if love.keyboard.isDown("w") then
-        moveY = moveY - 1
+        vectorY = -1
+        player.direction = 'up'
+        player.currentAnimation = player.animations.walk.up
+        isMoving = true
     end
 
     if love.keyboard.isDown("s") then
-        moveY = moveY +  1
-    end
-
-    if love.keyboard.isDown("a") then
-        moveX = moveX - 1
+        vectorY = 1
+        player.direction = 'down'
+        player.currentAnimation = player.animations.walk.down
+        isMoving = true
     end
 
     if love.keyboard.isDown("d") then
-        moveX = moveX + 1
+        vectorX = 1
+        player.direction = 'right'
+        player.currentAnimation = player.animations.walk.right
+        isMoving = true
     end
 
-    local length = math.sqrt(moveX^2 + moveY^2)
+    if love.keyboard.isDown("a") then
+        vectorX = -1
+        player.direction = 'left'
+        player.currentAnimation = player.animations.walk.left
+        isMoving = true
+    end
+
+    if isMoving == false then
+        if player.direction == 'up' then
+            player.currentAnimation = player.animations.idle.up
+
+        elseif player.direction == 'down' then
+            player.currentAnimation = player.animations.idle.down
+
+        elseif player.direction == 'right' then
+            player.currentAnimation = player.animations.idle.right
+
+        elseif player.direction == 'left' then
+            player.currentAnimation = player.animations.idle.left
+        end
+    end
+
+
+    local length = math.sqrt(vectorX^2 + vectorY^2)
 
     if length ~= 0 then
-        moveX = moveX/length
-        moveY = moveY/length
+        vectorX = vectorX/length
+        vectorY = vectorY/length
     end
 
-    player.x = player.x + moveX * player.speed * dt
-    player.y = player.y + moveY * player.speed * dt
+    player.collider:setLinearVelocity(vectorX * player.speed, vectorY * player.speed)
 
+    player.currentAnimation:update(dt)
+    world:update(dt)
+    gameMap:update(dt)
+    camera:lookAt(player.collider:getX(), player.collider:getY())
 end
 
 function love.draw()
-    love.graphics.draw(sprites.player, player.x, player.y, nil, 4 , nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2 )
+    --world:draw()
+    camera:attach()
+         
+        gameMap:drawLayer(gameMap.layers['BaseLayer'])
+        gameMap:drawLayer(gameMap.layers['DecorLayer'])
+        gameMap:drawLayer(gameMap.layers['TreeLayer'])
+        
+        player.currentAnimation:draw(player.spriteSheet, player.collider:getX(), player.collider:getY(), nil, 4 , nil, 16, 20)
+    camera:detach()
+    
+    
+end
+
+function loadMap(mapName)
+    gameMap = sti("maps/" .. mapName .. ".lua")
     
 end
