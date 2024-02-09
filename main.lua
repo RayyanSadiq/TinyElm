3571c387c647c4a5aa4df9c840739b842321ff1d
DragonXdroid <raysadik5@gmail.com>
2024-02-09 00:46:07 -0600

    Alpha Version 0.2 - Completed All bow animations - Added Arrows and Arrow colliders

--- main.lua
+++ main.lua
@@ -16,10 +16,10 @@ function love.load()
     world = windField.newWorld()
     world:addCollisionClass('Player')
     world:addCollisionClass('Boundry')
-
+    world:addCollisionClass('Arrow')
     
     player = {}
-    player.collider = world:newRectangleCollider(0,0,44,64, {collision_class = "Player" })
+    player.collider = world:newRectangleCollider(0,0,48,82, {collision_class = "Player" })
     player.collider:setFixedRotation(true)
     player.collider:setX(2700)
     player.collider:setY(2000)
@@ -55,10 +55,16 @@ function love.load()
 
     player.currentAnimation = player.animations.idle.down
 
+    arrows = {}
+
     boundries = {}
 
     loadMap("TutorialMap")
 end
+
+
+
+
 
 function love.update(dt)
 
@@ -117,27 +123,34 @@ function love.update(dt)
         vectorY = vectorY/length
     end
 
+    for index, arrow in ipairs(arrows) do
+        arrow.collider:setLinearVelocity(arrow.speed.x,arrow.speed.y)
+    end
+
     player.collider:setLinearVelocity(vectorX * player.speed, vectorY * player.speed)
 
 
     if love.mouse.isDown(1) then
         player.perfromingAction = true
 
+        if player.direction == 'up' then
+            player.currentAnimation = player.animations.bow.up
+
+        elseif player.direction == 'down' then
             player.currentAnimation = player.animations.bow.down
-            player.currentAnimation.status = "playing"
 
-            if player.currentAnimation.position == 3 then
-                player.isFullyDrawn = true  
-                player.currentAnimation:pause()    
-            end
+        elseif player.direction == 'right' then
+            player.currentAnimation = player.animations.bow.right
 
-        
-      
-        
+        elseif player.direction == 'left' then
+            player.currentAnimation = player.animations.bow.left
+        end
+
+        if player.currentAnimation.position == 3 then
+            player.isFullyDrawn = true  
+            player.currentAnimation:pause()    
+        end
     end
-
-     
-   
 
     player.currentAnimation:update(dt)
     world:update(dt)
@@ -146,30 +159,27 @@ function love.update(dt)
 
 end
 
+
+
+
 function love.draw()
     love.graphics.print(player.currentAnimation.position)
     
     camera:attach()
       
-        
         -- gameMap:drawLayer(gameMap.layers['BaseLayer'])
         gameMap:drawLayer(gameMap.layers['DecorLayer'])
         gameMap:drawLayer(gameMap.layers['TreeLayer'])
         world:draw()
 
-        player.currentAnimation:draw(player.spriteSheet, player.collider:getX(), player.collider:getY(), nil, 4 , nil, 16, 20)
+        player.currentAnimation:draw(player.spriteSheet, player.collider:getX(), player.collider:getY(), nil, 4 , nil, 15, 20)
+
+        for index, arrow in ipairs(arrows) do
+            love.graphics.draw(arrow.sprite, arrow.collider:getX() ,arrow.collider:getY(), angleFromDirection(arrow.direction), 3, nil, arrow.sprite:getWidth()/2, arrow.sprite:getHeight()/2)
+        end
 
     camera:detach()
     
-    
-end
-
-function loadMap(mapName)
-    gameMap = sti("maps/" .. mapName .. ".lua")
-    
-    for index, object in pairs(gameMap.layers["Boundries"].objects) do
-        makeBoundry(object.x, object.y, object.width, object.height )
-    end
 end
 
 function love.keypressed(key)
@@ -182,6 +192,10 @@ end
 
 function love.mousereleased( x, y, button, istouch, presses )
     if button == 1 then
+
+        if player.isFullyDrawn then
+            spawnArrow(player.direction)
+        end
         
         player.currentAnimation:gotoFrame(1)
         player.currentAnimation:resume()
@@ -192,10 +206,73 @@ function love.mousereleased( x, y, button, istouch, presses )
     end
 end
 
+function loadMap(mapName)
+    gameMap = sti("maps/" .. mapName .. ".lua")
+    
+    for index, object in pairs(gameMap.layers["Boundries"].objects) do
+        makeBoundry(object.x, object.y, object.width, object.height )
+    end
+end
+
+
 function makeBoundry(x, y, width, height) 
     if width > 0 and height > 0 then
         local boundry = world:newRectangleCollider(x, y, width, height, {collision_class = "Boundry"})
         boundry:setType("static")
         table.insert(boundries, boundry)
+    end
+end
+
+function spawnArrow(direction)
+    local arrow = {}
+    arrow.sprite = love.graphics.newImage('tiny-RPG-forest-files/PNG/sprites/misc/arrow.png')
+    arrow.direction = direction
+    arrow.speed = { x = 0, y = 0 }
+    arrow.scale = 3
+
+    -- Table to store directional settings
+    local directionalSettings = {
+        right = { xOffset = 24, yOffset = 4, speedX = 500 },
+        left = { xOffset = -80, yOffset = 4, speedX = -500 },
+        up = { xOffset = -4, yOffset = 88, speedY = -500 },
+        down = { xOffset = -4, yOffset = -40, speedY = 500 }
+    }
+
+    local settings = directionalSettings[direction]
+    if not settings then
+        error("Invalid direction: " .. direction)
+    end
+
+    -- Apply directional settings
+    arrow.speed.x = settings.speedX or 0
+    arrow.speed.y = settings.speedY or 0
+    arrow.playerXOffset = settings.xOffset
+    arrow.playerYOffset = settings.yOffset
+
+    -- Calculate dimensions
+    arrow.width = arrow.sprite:getWidth() * arrow.scale
+    arrow.height = arrow.sprite:getHeight() * arrow.scale
+
+    -- Create collider
+    arrow.collider = world:newRectangleCollider(player.collider:getX() + arrow.playerXOffset, player.collider:getY() - arrow.playerYOffset, arrow.width, arrow.height)
+    table.insert(arrows, arrow)
+    
+end
+
+
+function angleFromDirection(direction)
+
+    if direction == "up" then
+        return 0
+
+    elseif direction == "down" then
+        return math.pi
+
+    elseif direction == "right" then
+        return math.pi/2
+
+    elseif direction == "left" then
+        return -math.pi/2
+
     end
 end
